1be8c7cd9f6903b8c4712832f6d64a4685969191
salman <salmankhan.faze@gmail.com>
2023-11-24 10:57:26 +0530

    Making Nested Routes

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,6 @@
+const Developers=()=>{
+    return(
+        <h1>developers</h1>
+    )
+}
+export default Developers
---
+++ pages/news.js
@@ -0,0 +1,6 @@
+const NewsPage=()=>{
+    return(
+        <h1>News Page</h1>
+    )
+}
+export default NewsPage
